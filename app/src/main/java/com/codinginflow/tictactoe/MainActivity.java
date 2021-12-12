640eadaa2352435fee22a242766251e4b071d7ec
SyedShahidArshad <bitf18a528@pucit.edu.pk>
2021-12-12 12:08:59 +0500

    Started JAva coding

--- app/src/main/java/com/codinginflow/tictactoe/MainActivity.java
+++ app/src/main/java/com/codinginflow/tictactoe/MainActivity.java
@@ -9,3 +9,17 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mypractice002.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[3][3];
+
+    private boolean player1Turn = true;
+
+    private int roundCount;
+
+    private int player1Points;
+    private int player2Points;
+
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
